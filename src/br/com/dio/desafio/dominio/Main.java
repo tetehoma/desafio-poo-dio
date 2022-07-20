6ce94dd3594982189886d07ca9503eacb6fe0fc1
tetehoma <tetehoma@gmail.com>
2022-07-20 10:17:43 -0300

    Mentoria e curso

---
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,30 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        Curso curso2 = new Curso();
+
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descricao curso Java");
+        curso1.setCargaHoraria(8);
+
+        curso2.setTitulo("curso Java");
+        curso2.setDescricao("descricao curso Java");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descricao mentoria de java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+
+
+}
